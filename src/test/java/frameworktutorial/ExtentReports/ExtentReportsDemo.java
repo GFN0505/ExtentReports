ecb5074f923e23e263e2f97f23e50aad47372be4
Favour Nwoko <nwokogodsfavour2@gmail.com>
2024-06-19 16:27:22 +0100

    stable

---
+++ src/test/java/frameworktutorial/ExtentReports/ExtentReportsDemo.java
@@ -0,0 +1,55 @@
+package frameworktutorial.ExtentReports;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ExtentReportsDemo {
+	
+	
+	ExtentReports extent;
+	
+	@BeforeTest
+	public void config()
+	{
+		//ExtentReports,  ExtentSparkReporter
+		String path = System.getProperty("user.dir") + "//reports//new.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Results");
+		reporter.config().setDocumentTitle("Test Results");
+		
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Godsfavour Nwoko");
+		
+	}
+	
+	
+	@Test
+	public void initialDemo()
+	{
+		ExtentTest test = extent.createTest("InitialDemo");
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		test.fail("results do not match");
+		
+		extent.flush();
+		driver.quit();	
+		
+	}
+
+}
